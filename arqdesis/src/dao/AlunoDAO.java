bf71a442317f0465ecef5da94683d4ae6b0687ef
William Marques <imarxwill@gmail.com>
2016-03-15 18:09:41 -0300

    exercicios aula 3

---
+++ arqdesis/src/dao/AlunoDAO.java
@@ -0,0 +1,94 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class AlunoDAO {
+		public void incluir(AlunoTO to) {
+		String sqlInsert = "Insert Into aluno (nome, sexo, dataNascimento, endereco, numero, complemento, telefone, email, rg, cpf) values (?,?,?,?,?,?,?,?,?,?)";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlInsert);) {
+			stm.setInt(1, to.getCodigo());
+			stm.setString(2, to.getNome());
+         stm.setString(3, to.getSexo());
+         stm.setString(4, to.getdataNascimento());
+         stm.setString(5, to.getEndereco());
+         stm.setInt(6, to.getNumero());
+         stm.setString(7, to.getComplemento());
+         stm.setString(8, to.getFone());
+         stm.setString(9, to.getEmail());
+         stm.setString(10, to.getRg());
+         stm.setString(11, to.getCPF());
+  			stm.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+  	public void alterar(AlunoTO to) {
+		 String sqlUpdate = "update aluno set nome = ?, sexo = ?, dataNascimento = ?, endereco = ?, numero = ?, complemento = ?, telefone = ?, email = ?, rg = ?, cpf = ? where codigo = ?"
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlUpdate);) {
+			stm.setString(1, to.getNome());
+         stm.setString(2, to.getSexo());
+         stm.setString(3, to.getdataNascimento());
+         stm.setString(4, to.getEndereco());
+         stm.setInt(5, to.getNumero());
+         stm.setString(6, to.getComplemento());
+         stm.setString(7, to.getFone());
+         stm.setString(8, to.getEmail());
+         stm.setString(9, to.getRg());
+         stm.setString(10, to.getCPF());
+         stm.setInt(11, to.getCodigo());
+			stm.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void deletar(AlunoTO to) {
+		String sqlDelete = "DELETE FROM aluno WHERE codigo = ?";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlDelete);) {
+			stm.setInt(1, to.getCodigo());
+			stm.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public AlunoTO carregar(int codigo) {
+		AlunoTO to = new AlunoTO();
+		String sqlSelect = "SELECT * FROM aluno WHERE aluno.codigo = ?";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
+			stm.setInt(1, codigo);
+			try (ResultSet rs = stm.executeQuery();) {
+				if (rs.next()) {
+					to.setNome(rs.getString("nome"));
+               to.setSexo(rs.getString("sexo"));
+               to.setDataNascimento(rs.getString("dataNascimento"));
+               to.setEndereco(rs.getString("endereco"));
+               to.setNumero(rs.getInt("numero"));
+               to.setComplemento(rs.getString("complemento"));
+					to.setFone(rs.getString("fone"));
+               to.setEmail(rs.getString("email"));
+               to.setRg(rs.getString("rg"));
+               to.setCPF(rs.getString("cpf"));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e1) {
+			System.out.print(e1.getStackTrace());
+		}
+		return to;
+	}
+}
---
+++ arqdesis/src/to/CursoTO.java
@@ -0,0 +1,123 @@
+package to;
+
+public class CursoTO {
+	private int codigo, numeroDeVagas;
+	private String nome, tipo, dataDeInicio, dataDeTermino, horario, tipoLab, softwares, livros, descricaoMaterial;
+	private double valor;
+   
+	public int getCodigo(){
+		   
+	      return codigo;   
+	   }
+	   
+	   public String getNome(){
+	      
+	      return nome;    
+	   }
+	   
+	   public String getTipo(){
+	   
+	      return tipo;
+	   }
+	   
+	   public String getDataDeInicio(){
+	   
+	      return dataDeInicio;
+	   }
+	   
+	   public String getDataDeTermino(){
+	      
+	      return dataDeTermino;
+	   }
+	   
+	   public String getHorario(){
+	   
+	      return horario;
+	   }
+	   
+	   public int getNumeroDeVagas(){
+	   
+	      return numeroDeVagas;
+	   }
+	   
+	   public double getValor(){
+	   
+	      return valor;
+	   }
+	 
+	   public String getTipoLab(){
+	   
+	      return tipoLab;
+	   }
+	   
+	   public String getSoftwares(){
+	   
+	      return softwares;
+	   }
+	   
+	   public String getLivros(){
+	   
+	      return livros;
+	   }
+	   
+	   public String getDescricaoMaterial(){
+	   
+	      return descricaoMaterial;
+	   }
+	   
+	   public void setNome(String nome){
+	   
+	      this.nome = nome;
+	   }
+	   
+	   public void setTipo(String tipo){
+	   
+	      this.tipo = tipo;
+	   }
+	   
+	   public void setDataDeInicio(String dataDeInicio){
+	   
+		   this.dataDeInicio = dataDeInicio;
+	   }
+	   
+	   public void setDataDeTermino(String dataDeTermino){
+	   
+		   this.dataDeTermino = dataDeTermino;
+	   }
+	   
+	   public void setHorario(String horario){
+	   
+		   this.horario = horario;
+	   }
+	   
+	   public void setNumeroDeVagas(int numeroDeVagas){
+	   
+		   this.numeroDeVagas = numeroDeVagas;
+	   }  
+	   
+	   public void setValor(double valor){
+	   
+		   this.valor = valor;
+	   }
+	   
+	   public void setTipoLab(String tipoLab){
+	   
+		   this.tipoLab = tipoLab;
+	   }
+	   
+	   public void setSoftwares(String softwares){
+	   
+		   this.softwares = softwares;
+	   }
+	   
+	   public void setLivros(String livros){
+	   
+		   this.livros = livros;
+	   }
+	   
+	   public void setDescricaoMaterial(String descricaoMaterial){
+	   
+		   this.descricaoMaterial = descricaoMaterial;
+	   }
+}
+
